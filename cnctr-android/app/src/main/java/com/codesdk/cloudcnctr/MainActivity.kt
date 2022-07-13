6285fce6d7f55094ba98667c7d749eaf2559367e
Arunabh Das <arunabhdas@gmail.com>
2022-07-12 23:55:29 -0400

    Added new ic_launcher icon

--- cnctr-android/app/src/main/java/com/codesdk/cloudcnctr/MainActivity.kt
+++ cnctr-android/app/src/main/java/com/codesdk/cloudcnctr/MainActivity.kt
@@ -10,40 +10,18 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.codesdk.cloudcnctr.presentation.ui.Screen
-import com.codesdk.cloudcnctr.presentation.ui.CryptoListScreen
-import com.codesdk.cloudcnctr.presentation.ui.CryptoDetailScreen
-
 import com.codesdk.cloudcnctr.presentation.ui.theme.CloudCnctrTheme
-import dagger.hilt.android.AndroidEntryPoint
+import com.codesdk.cloudcnctr.presentation.ui.MainScreen
+import com.codesdk.cloudcnctr.presentation.ui.TabOneScreen
 
-@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     @ExperimentalFoundationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CloudCnctrTheme {
-                Surface(color = MaterialTheme.colors.background) {
-                    val navController = rememberNavController()
-                    NavHost(
-                        navController = navController,
-                        startDestination = Screen.MainScreen.route
-                    ) {
-                        composable(
-                            route = Screen.CryptoListScreen.route
-                        ) {
-                            CryptoListScreen(navController)
-                        }
-                        composable(
-                            route = Screen.CryptoDetailScreen.route + "/{cryptoId}"
-                        ) {
-                            CryptoDetailScreen()
-                        }
-                    }
-                }
+                MainScreen()
             }
         }
     }
